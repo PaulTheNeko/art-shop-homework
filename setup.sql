460354969c182b07d9bf154f185821c206cd01ec
Paweł Nowiński <tygrysekpawelpiotr1@gmail.com>
2022-06-05 22:44:52 +0200

    simplification no accounts no artists

--- setup.sql
+++ setup.sql
@@ -24,23 +24,6 @@ create table brands (
   PRIMARY KEY (id)
 );
 
-create table artists (
-  id int not null auto_increment,
-  pseudonym varchar(100) not null,
-  `desc` text not null,
-  PRIMARY KEY (id)
-);
-
--- Account for putting up sales
-create table vendors (
-  id int not null auto_increment,
-  `login` varchar(20) not null,
-  email varchar(255) not null,
-  pass_hash char(60) not null, -- bcrypt bcrypt bcrypt
-  artist int,
-  PRIMARY KEY (id)
-);
-
 create table paintings (
   id int not null auto_increment,
   -- cena 
@@ -48,18 +31,12 @@ create table paintings (
   `desc` text not null,
   paint_type int,
   paint_brand int,
-  artist int,
-  vendor -- don't show to buyers
-    int NOT NULL, -- _SOMEONE_ has to put the paintings on sale
   height int,
   width int,
   depth int,
   PRIMARY KEY (id),
   foreign key (paint_type) references paint_types(id),
-  foreign key (paint_brand) references brands(id),
-  foreign key (artist) references artists(id),
-  foreign key (vendor) references vendors(id)
-  -- FOREIGN KEY REFERENCES paint_types(id)
+  foreign key (paint_brand) references brands(id)
 );
 
 create table purchases (
